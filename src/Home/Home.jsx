2dbc53991ad125f8bda7562dce466b4d2350505b
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-03-11 23:15:43 +0530

    New CHanges in the sidebar and Feed also have many update for day

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-
+import "./Home.css";
 import Sidebar from "../SideBar/Sidebar.jsx";
+import Feed from "../Feed/Feed.jsx";
 
 const Home = ({ sidebar }) => {
   return (
     <>
       <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   );
 };
